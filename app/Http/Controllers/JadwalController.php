2cc9e145bff4edf31a2a104c2af55c6be80168a2
hansarwa2406 <kuronokameraman@gmail.com>
2021-12-12 16:38:40 +0700

    Update JadwalController.php

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Jadwal;
 use Illuminate\Http\Request;
 
-class JadwalControllers extends Controller
+class JadwalController extends Controller
 {
     /**
      * Display a listing of the resource.
