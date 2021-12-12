4be90e5b5902b2bfac8ef5cad65e92f931706cca
Nisa Hayuningrum <nisahayuningrum.nh@gmail.com>
2021-12-12 21:53:38 +0700

    Add login and register

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.main')
+
+@section('container')
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <main class="form-signin">
+                <h1 class="h3 mb-3 fw-normal text-center my-3">Sign In</h1>
+
+                @if (session('status'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+                        {{ session('status') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
+
+                @if (session('loginError'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                        {{ session('loginError') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
+
+                <form class="mt-4" action="/login" method="POST">
+                    @csrf
+                    <div class="form-floating">
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" value="{{ old('email') }}" autofocus required>
+                        <label for="email">Email address</label>
+                        @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+                        <label for="password">Password</label>
+                    </div>
+
+                    <button class="w-100 btn btn-lg btn-primary my-3" type="submit">Login</button>
+                </form>
+                
+                <small>Not registered? <a href="/register" class="text-decoration-none">Sign Up Now!</a></small>
+            </main>
+        </div>
+    </div>
+@endsection
+{{-- @extends('layouts.main')
+
+@section('container')
+<div class="row justify-content-center">
+    <div class="col-md-4">
+
+      @if (session()->has('success'))
+          <div class="alert alert-success alert-dismissible fade show" role="alert">
+       {{ session('success') }}
+        <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">
+        </button>
+      </div>
+      @endif
+      
+      @if (session()->has('loginError'))
+          <div class="alert alert-danger alert-dismissible fade show" role="alert">
+       {{ session('loginError') }}
+        <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">
+        </button>
+      </div>
+      @endif
+      
+        <main class="form-signin">
+            <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
+            <form action="/login" method="POST">
+              @csrf
+          
+              <div class="form-floating">
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus value="{{ old('email') }}">
+                <label for="email">Email address</label>
+                @error('email')
+                    <div class="invalid-feedback">
+                      {{ $message }}
+                    </div>
+                @enderror
+              </div>
+              <div class="form-floating">
+                <input type="name" class="form-control" id="password" placeholder="Password" required>
+                <label for="password">Password</label>
+              </div>
+          
+              <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+            </form>
+            <small class="d-block text-center mt-3">Not Registered? <a href="/register">Register</a></small>
+          </main>
+    </div>
+</div>
+
+@endsection --}}
