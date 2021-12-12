d9e1ee77f041a5785d77d1eed6f146d895326b7f
hansarwa2406 <kuronokameraman@gmail.com>
2021-12-12 14:31:47 +0700

    update seeder

--- database/seeders/DosenSeeder.php
+++ database/seeders/DosenSeeder.php
@@ -14,6 +14,40 @@ class DosenSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('dosens')->insert([
+            'nama' => 'Abdul Barir',
+            'nidn' => '57201',
+            'alamat' => 'Depok',
+            'kontak' => '08211654732',
+            
+        ]);
+        DB::table('dosens')->insert([
+            'nama' => 'Asri Pertiwi',
+            'nidn' => '57202',
+            'alamat' => 'Jakarta Selatan',
+            'kontak' => '08211654732',
+            
+        ]);
+        DB::table('dosens')->insert([
+            'nama' => 'Erza Sofian',
+            'nidn' => '57203',
+            'alamat' => 'Jakarta Selatan',
+            'kontak' => '08211654786',
+            
+        ]);
+        DB::table('dosens')->insert([
+            'nama' => 'Ahlijati Nuraminah',
+            'nidn' => '55021',
+            'alamat' => 'Jakarta Timur',
+            'kontak' => '08211655432',
+            
+        ]);
+        DB::table('dosens')->insert([
+            'nama' => 'Desy Komalasari',
+            'nidn' => '55022',
+            'alamat' => 'Jakarta Barat',
+            'kontak' => '08211656567',
+            
+        ]);
     }
 }
--- database/seeders/MahasiswaSeeder.php
+++ database/seeders/MahasiswaSeeder.php
@@ -14,6 +14,21 @@ class MahasiswaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('mahasiswas')->insert([
+            'nama' => 'Sara Febriana',
+            'nim' => '1810120019',
+            'tanggal_lahir' => '1994-02-11',
+            'alamat' => 'Depok',
+            'tahun_masuk' => '2014',
+            
+        ]);
+        DB::table('mahasiswas')->insert([
+            'nama' => 'Fatiyyah Karimah',
+            'nim' => '1810120010',
+            'tanggal_lahir' => '2000-01-12',
+            'alamat' => 'Bogor',
+            'tahun_masuk' => '2018',
+            
+        ]);
     }
 }
