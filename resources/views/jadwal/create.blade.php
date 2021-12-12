2b20d305b74f2b35f2895d081e8b0d586d0099bf
hansarwa2406 <kuronokameraman@gmail.com>
2021-12-12 16:34:04 +0700

    jadwal changes

--- resources/views/jadwal/create.blade.php
+++ resources/views/jadwal/create.blade.php
@@ -1,75 +1,73 @@
 @extends('layouts.main')
 
 @section('container')
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Tambah Jadwal Bimbingan</h1>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Tambah Jadwal Bimbingan</h1>
+  </div>
+  <div class="tools p-2 rounded-3">
+    <a href="{{ route('jadwal.index') }}" class="btn btn-secondary mb-3"><span data-feather="arrow-left"></span> Kembali</a>
+  </div>
+  <div class="col-lg-8">
+    <form action="{{ route('jadwal.store') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+          <label for="mahasiswa_id" class="form-label">Mahasiswa</label>
+          <input type="text" class="form-control @error('mahasiswa_id') is-invalid @enderror" id="mahasiswa_id" name="mahasiswa_id" value="{{ old('mahasiswa_id') }}" placeholder="Mahasiswa ID" required autofocus>
+          @error('mahasiswa_id')
+              <div class="invalid-feedback">
+                  {{ $message }}
+              </div>
+          @enderror
+      </div>
+        <div class="mb-3">
+          <label for="dosen_id" class="form-label">Dosen</label>
+          <input type="text" class="form-control @error('dosen_id') is-invalid @enderror" id="dosen_id" name="dosen_id" value="{{ old('dosen_id') }}" placeholder="Dosen ID" required autofocus>
+          @error('dosen_id')
+              <div class="invalid-feedback">
+                  {{ $message }}
+              </div>
+          @enderror
+      </div>
+      <div class="mb-3">
+        <label for="judul" class="form-label">Judul</label>
+        <input type="text" class="form-control @error('judul') is-invalid @enderror" id="judul" name="judul" value="{{ old('judul') }}" placeholder="Judul" required autofocus>
+        @error('judul')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
     </div>
-
-    <div class="col-lg-5 mb-5">
-        <form method="POST" action="/jadwal" enctype="multipart/form-data">
-            @csrf
-            <div class="mb-3">
-                <label for="mahasiswa_id" class="form-label">Mahasiswa</label>
-                <select class="form-select" name="mahasiswa_id">
-                    @foreach ($mahasiswa as $student)
-                        @if (old('mahasiswa_id') == $student->id)
-                            <option value="{{ $student->id}}" selected>{{ $student->nama }}</option>
-                        @else
-                            <option value="{{ $student->id}}">{{ $student->nama }}</option>
-                        @endif
-                    @endforeach
-                </select>
+    <div class="mb-3">
+        <label for="deskripsi" class="form-label">Deskripsi</label>
+        <textarea class="form-control @error('deskripsi') is-invalid @enderror" style="height:150px" name="deskripsi" placeholder="Deskripsi Bimbingan" id="deskripsi" name="deskripsi" value="{{ old('deskripsi') }}" required autofocus></textarea>
+        @error('deskripsi')
+            <div class="invalid-feedback">
+                {{ $message }}
             </div>
-            <div class="mb-3">
-                <label for="dosen_id" class="form-label">Dosen</label>
-                <select class="form-select" name="dosen_id">
-                    @foreach ($dosen as $lecturer)
-                        @if (old('dosen_id') == $lecturer->id)
-                            <option value="{{ $lecturer->id}}" selected>{{ $lecturer->nama }}</option>
-                        @else
-                            <option value="{{ $lecturer->id}}">{{ $lecturer->nama }}</option>
-                        @endif
-                    @endforeach
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="judul" class="form-label">Judul</label>
-                <input type="text" class="form-control @error('judul') is-invalid @enderror" id="judul" name="judul" value="{{ old('judul') }}" required autofocus>
-                @error('judul')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="deksripsi" class="form-label">Deskripsi</label>
-                <input type="text" class="form-control @error('deksripsi') is-invalid @enderror" id="deksripsi" name="deksripsi" value="{{ old('deksripsi') }}" required>
-                @error('deksripsi')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="awal" class="form-label">Awal</label>
-                <input type="datetime-local" class="form-control @error('awal') is-invalid @enderror" id="awal" name="awal" value="{{ old('awal') }}" required>
-                @error('awal')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="akhir" class="form-label">Akhir</label>
-                <input type="datetime-local" class="form-control @error('akhir') is-invalid @enderror" id="akhir" name="akhir" value="{{ old('akhir') }}" required>
-                @error('akhir')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-                @enderror
-            </div>
-
-            <button type="submit" class="btn btn-primary">Tambah</button>
-        </form>
+        @enderror
     </div>
+        <div class="mb-3">
+          <label for="awal" class="form-label">Awal</label>
+          <input type="datetime-local" class="form-control @error('awal') is-invalid @enderror" id="awal" name="awal" value="{{ old('awal') }}" placeholder="Awal" required autofocus>
+          @error('awal')
+              <div class="invalid-feedback">
+                  {{ $message }}
+              </div>
+          @enderror
+      </div>
+        <div class="mb-3">
+          <label for="akhir" class="form-label">Akhir</label>
+          <input type="datetime-local" class="form-control @error('akhir') is-invalid @enderror" id="akhir" name="akhir" value="{{ old('akhir') }}" placeholder="Akhir" required autofocus>
+          @error('akhir')
+              <div class="invalid-feedback">
+                  {{ $message }}
+              </div>
+          @enderror
+      </div>
+     
+      <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+  </div>
+ 
+
 @endsection
--- resources/views/jadwal/index.blade.php
+++ resources/views/jadwal/index.blade.php
@@ -6,19 +6,19 @@
     </div>
 
     @if (session('success'))
-        <div class="alert alert-success alert-dismissible fade show col-lg-5" role="alert">
+        <div class="alert alert-success alert-dismissible fade show col-lg-8" role="alert">
             <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
             {{ session('success') }}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
 
-    <div class="table-responsive col-lg-8">
-        <a href="/jadwal/create" class="btn btn-primary mb-3"><span data-feather="plus-circle"></span> Tambah Jadwal</a>
+    <div class="table-responsive col-lg-10">
+        <a href="{{ route('jadwal.create') }}" class="btn btn-primary mb-3"><span data-feather="plus-circle"></span> Tambah Jadwal Bimbingan</a>
         <table class="table table-striped table-sm">
             <thead>
                 <tr class=" text-center">
-                    <th scope="col">No.</th>
+                    <th scope="col">No</th>
                     <th scope="col">Mahasiswa</th>
                     <th scope="col">Dosen</th>
                     <th scope="col">Judul</th>
@@ -32,19 +32,22 @@
                 @foreach ($jadwal as $schedule)
                     <tr>
                         <td class=" text-center">{{ $loop->iteration }}</td>
-                        <td>{{ $schedule->mahasiswa }}</td>
-                        <td>{{ $schedule->dosen }}</td>
+                        <td>{{ $schedule->mahasiswa_id }}</td>
+                        <td>{{ $schedule->dosen_id }}</td>
                         <td>{{ $schedule->judul }}</td>
                         <td>{{ $schedule->deskripsi }}</td>
                         <td>{{ $schedule->awal }}</td>
                         <td>{{ $schedule->akhir }}</td>
                         <td class=" text-center">
-                            <a href="/jadwal/{{ $schedule->id }}" class="badge bg-info text-decoration-none" title="View"><span data-feather="eye"></a>
-                            <a href="/jadwal/{{ $schedule->id }}/edit" class="badge bg-warning text-decoration-none" title="Edit"><span data-feather="edit"></a>
-                            <form action="/jadwal/{{ $schedule->id }}" method="POST" class=" d-inline">
-                                @method('delete')
+                            <form action="{{ route('jadwal.destroy',$schedule->id) }}" method="POST" class=" d-inline">
+                                <a class="badge bg-info text-decoration-none" title="View" href="{{ route('jadwal.show',$schedule->id) }}"><span data-feather="eye"></a>
+
+                                <a class="badge bg-warning text-decoration-none" title="Edit" href="{{ route('jadwal.edit',$schedule->id) }}"><span data-feather="edit"></a>
+            
                                 @csrf
-                                <button class="badge bg-danger border-0" title="Delete" onclick="return confirm('Are you sure you want to delete this post: {{ $schedule->title }}? ')"><span data-feather="trash-2"></button>
+                                @method('DELETE')
+            
+                                <button type="submit" class="badge bg-danger border-0" title="Delete" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')"><span data-feather="trash-2"></button>
                             </form>
                         </td>
                     </tr>
@@ -52,4 +55,5 @@
             </tbody>
         </table>
     </div>
+    {!! $jadwal->links() !!}
 @endsection
