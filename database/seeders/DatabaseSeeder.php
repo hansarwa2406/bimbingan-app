6b251d90cd04f9b533ca1fcee1f829d6568ad46e
hansarwa2406 <kuronokameraman@gmail.com>
2021-12-13 22:24:01 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Han Sarwa',
+            'username' => 'hansarwa',
+            'email' => 'hansarwa@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
     }
 }
